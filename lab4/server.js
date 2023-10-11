bcaea117e55e13df512c793dcc935e57ae117013
NadiiaBul <nadjabulbotka@gmail.com>
2023-10-11 11:50:29 +0300

    created lab4

---
+++ lab4/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat message', (data) => {
+    io.emit('chat message', {name: data.name, message: data.message, color: data.color});
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
